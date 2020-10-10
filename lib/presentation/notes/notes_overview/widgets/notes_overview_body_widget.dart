3540d019f2a75f57aecea612398edebd86860403
ggguille <g.granado.g@gmail.com>
2020-10-10 16:52:41 +0200

    Implement Note Card widget

--- lib/presentation/notes/notes_overview/widgets/notes_overview_body_widget.dart
+++ lib/presentation/notes/notes_overview/widgets/notes_overview_body_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_notes_app/application/notes/note_watcher/note_watcher_block.dart';
 import 'package:flutter_notes_app/application/notes/note_watcher/note_watcher_state.dart';
+import 'package:flutter_notes_app/presentation/notes/notes_overview/widgets/note_card_widget.dart';
 
 class NotesOverviewBody extends StatelessWidget {
   @override
@@ -25,10 +26,8 @@ class NotesOverviewBody extends StatelessWidget {
                   );
                 }
                 // TODO Note Card
-                return Container(
-                  color: Colors.green,
-                  width: 100,
-                  height: 100,
+                return NoteCard(
+                  note: note,
                 );
               },
               itemCount: state.notes.length(),
