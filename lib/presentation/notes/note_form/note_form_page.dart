152af913d74fa120e926ac3e545b5eeecef6eb80
ggguille <g.granado.g@gmail.com>
2020-10-18 20:31:39 +0200

    Implement error handling and saving indicator

--- lib/presentation/notes/note_form/note_form_page.dart
+++ lib/presentation/notes/note_form/note_form_page.dart
@@ -1,4 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flushbar/flushbar.dart';
+import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_notes_app/application/notes/note_form/note_form_bloc.dart';
@@ -6,6 +9,7 @@ import 'package:flutter_notes_app/application/notes/note_form/note_form_event.da
 import 'package:flutter_notes_app/application/notes/note_form/note_form_state.dart';
 import 'package:flutter_notes_app/domain/notes/note.dart';
 import 'package:flutter_notes_app/injection.dart';
+import 'package:flutter_notes_app/presentation/routes/router.gr.dart';
 
 class NoteFormPage extends StatelessWidget {
   final Note editingNote;
@@ -20,13 +24,95 @@ class NoteFormPage extends StatelessWidget {
     return BlocProvider(
       create: (context) => getIt<NoteFormBloc>()
         ..add(NoteFormEvent.initialized(optionOf(editingNote))),
-      child: const NotePageScafold(),
+      child: BlocConsumer<NoteFormBloc, NoteFormState>(
+        listenWhen: (previous, current) =>
+            previous.saveFailureOrSuccessOption !=
+            current.saveFailureOrSuccessOption,
+        listener: (context, state) {
+          state.saveFailureOrSuccessOption.fold(
+            () {},
+            (either) {
+              either.fold(
+                (failure) {
+                  FlushbarHelper.createError(
+                    message: failure.map(
+                      unexpected: (_) =>
+                          'Unexpected error occured, please contact support.',
+                      insufficientPermissions: (_) =>
+                          'Insufficient permissions ❌',
+                      unableToUpdate: (_) =>
+                          "Couldn't update the note. Was it deleted from another device?",
+                    ),
+                  ).show(context);
+                },
+                (_) {
+                  ExtendedNavigator.of(context).popUntil(
+                    (route) => route.settings.name == Routes.notesOverviewPage,
+                  );
+                },
+              );
+            },
+          );
+        },
+        buildWhen: (previous, current) => previous.isSaving != current.isSaving,
+        builder: (context, state) {
+          return Stack(
+            children: [
+              const NoteFormPageScafold(),
+              SavingInProgressOverlay(
+                isSaving: state.isSaving,
+              )
+            ],
+          );
+        },
+      ),
     );
   }
 }
 
-class NotePageScafold extends StatelessWidget {
-  const NotePageScafold({
+class SavingInProgressOverlay extends StatelessWidget {
+  final bool isSaving;
+
+  const SavingInProgressOverlay({
+    Key key,
+    @required this.isSaving,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IgnorePointer(
+      ignoring: !isSaving,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 150),
+        color: isSaving ? Colors.black.withOpacity(0.8) : Colors.transparent,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Visibility(
+          visible: isSaving,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircularProgressIndicator(),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                'Saving',
+                style: Theme.of(context).textTheme.bodyText2.copyWith(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NoteFormPageScafold extends StatelessWidget {
+  const NoteFormPageScafold({
     Key key,
   }) : super(key: key);
 
