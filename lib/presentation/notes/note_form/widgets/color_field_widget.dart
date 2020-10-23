f647d36ba8cf347414cd73ce828ed1c3f095d635
ggguille <g.granado.g@gmail.com>
2020-10-23 20:02:08 +0200

    Bind Note to ColorField and remark its color

--- lib/presentation/notes/note_form/widgets/color_field_widget.dart
+++ lib/presentation/notes/note_form/widgets/color_field_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_notes_app/application/notes/note_form/note_form_bloc.dart';
+import 'package:flutter_notes_app/application/notes/note_form/note_form_state.dart';
 import 'package:flutter_notes_app/domain/notes/value_objects.dart';
 
 class ColorField extends StatelessWidget {
@@ -8,35 +11,46 @@ class ColorField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      child: ListView.separated(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 8,
-        ),
-        physics: const BouncingScrollPhysics(),
-        scrollDirection: Axis.horizontal,
-        itemCount: NoteColor.predefinedColors.length,
-        itemBuilder: (context, index) {
-          final itemColor = NoteColor.predefinedColors[index];
-          return Material(
-            color: itemColor,
-            elevation: 4,
-            shape: const CircleBorder(
-              side: BorderSide.none,
+    return BlocBuilder<NoteFormBloc, NoteFormState>(
+      buildWhen: (previous, current) =>
+          previous.note.color != current.note.color,
+      builder: (context, state) {
+        return Container(
+          height: 80,
+          child: ListView.separated(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 8,
             ),
-            child: Container(
-              width: 50,
-              height: 50,
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return const SizedBox(
-            width: 12,
-          );
-        },
-      ),
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            itemCount: NoteColor.predefinedColors.length,
+            itemBuilder: (context, index) {
+              final itemColor = NoteColor.predefinedColors[index];
+              return Material(
+                color: itemColor,
+                elevation: 4,
+                shape: CircleBorder(
+                  side: state.note.color.value.fold(
+                    (_) => BorderSide.none,
+                    (color) => color == itemColor
+                        ? const BorderSide(width: 1.5)
+                        : BorderSide.none,
+                  ),
+                ),
+                child: Container(
+                  width: 50,
+                  height: 50,
+                ),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return const SizedBox(
+                width: 12,
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
